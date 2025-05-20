d5d57f7fa165b5a397986a30a8e68497c1e3b664
Adwait Mane <mane.learn.2023.04@gmail.com>
2025-05-19 20:37:05 -0400

    Ongoing. Update geometry, mass, and inertias.

--- 2025-walter-sr-leg/getVehicleParams.m
+++ 2025-walter-sr-leg/getVehicleParams.m
@@ -2,58 +2,72 @@
 
 function params = getVehicleParams()
 
+% SI units. Meters, kilograms, seconds.
+
 % Gravity
 params.g = 9.81;
 
+% ---------- Walter Sr. values ----------
+
 % Wheel and terrain geometry.
-params.wheel_radius = 0.75 ; 
+params.wheel_radius = 0.064 ; 
 params.terrain_radius = 5 ; 
 
 % Main Body (1)
-params.m1 = 1; % Mass of body 1
-params.L1a = 1.0;  % Length to front joint
-params.L1b = 1.0;  % length to back joint
-params.I1 = 1; % inertia
+params.m1 = 2.5; % Mass of body 1
+params.L1a = 0.02;  % Length to front joint
+params.I1 = (1/12)*params.m1*params.L1a^2; % inertia
 
 % Front Thigh (2)
-params.m2 = 1; % Mass of body 1
-params.L2 = 2;  % Length to front wheel
-params.I2 = 1; % inertia
+params.m2 = 0.4; % Mass of body 2
+params.L2 = 0.102;  % Length to front wheel
+params.I2 = (1/12)*params.m2*params.L2^2; % inertia
 
 % Front Shin (3)
-params.m3 = 1; % Mass of body 1
-params.L3a = 1;  % Length to front joint
-params.L3b = 1;  % length to back joint
-params.I3 = 1/3; % inertia
+params.m3 = 1.3; % Mass of body 1
+params.L3a = 0.159/2;  % Length to front joint
+params.L3b = 0.159/2;  % length to back joint
+params.I3 = (1/12)*params.m3*(params.L3a+params.L3b)^2; % inertia
 
 % Front Wheel A (4)
 params.m4 = 1; % Mass of body 1
-params.L4 = 0.75;  % Length to front wheel
+params.L4 = params.wheel_radius;  % Length to front wheel
 params.I4 = 1/3; % inertia
 
 % Front Wheel B (5)
 params.m5 = 1; % Mass of body 1
-params.L5 = 0.75;  % Length to front wheel
+params.L5 = params.wheel_radius;  % Length to front wheel
 params.I5 = 1/3; % inertia
 
+% ---------- Arbitrary values ----------
 
-% Back Thigh (6)
-params.m6 = 1; % Mass of body 1
-params.L6 = 2;  % Length to front wheel
-params.I6 = 1/3; % inertia
-
-% Back Shin (7)
-params.m7 = 1; % Mass of body 1
-params.L7a = 1;  % Length to front joint
-params.L7b = 1;  % length to back joint
-params.I7 = 1/3; % inertia
-
-% Back Wheel A (8)
-params.m8 = 1; % Mass of body 1
-params.L8 = 0.75;  % Length to front wheel
-params.I8 = 1/3; % inertia
-
-% Back Wheel B (9)
-params.m9 = 1; % Mass of body 1
-params.L9 = 0.75;  % Length to front wheel
-params.I9 = 1/3; % inertia
+% % Wheel and terrain geometry.
+% params.wheel_radius = 0.75 ; 
+% params.terrain_radius = 5 ; 
+% 
+% % Main Body (1)
+% params.m1 = 1; % Mass of body 1
+% params.L1a = 1.0;  % Length to front joint
+% params.L1b = 1.0;  % length to back joint
+% params.I1 = 1; % inertia
+% 
+% % Front Thigh (2)
+% params.m2 = 1; % Mass of body 1
+% params.L2 = 2;  % Length to front wheel
+% params.I2 = 1; % inertia
+% 
+% % Front Shin (3)
+% params.m3 = 1; % Mass of body 1
+% params.L3a = 1;  % Length to front joint
+% params.L3b = 1;  % length to back joint
+% params.I3 = 1/3; % inertia
+% 
+% % Front Wheel A (4)
+% params.m4 = 1; % Mass of body 1
+% params.L4 = params.wheel_radius;  % Length to front wheel
+% params.I4 = 1/3; % inertia
+% 
+% % Front Wheel B (5)
+% params.m5 = 1; % Mass of body 1
+% params.L5 = params.wheel_radius;  % Length to front wheel
+% params.I5 = 1/3; % inertia
